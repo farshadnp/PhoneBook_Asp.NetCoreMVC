165e8b6d5caaa77ff66300ec00616f45a648aaf0
farshad <farshadnp6@gmail.com>
2022-01-12 11:01:37 +0330

    1. update person model with new properties. 2. add DatabaseContext 3. add migration 4. Configure Services in startup.cs  for use sql server middleware.

--- PhoneBook/Models/DataBaseContext/PhoneBookContext.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Data.Entity;
-
-namespace PhoneBook.Models.DataBaseContext
-{
-    public class PhoneBookContext : DbContext
-    {
-        
-    }
-}
--- PhoneBook/Models/Person.cs
+++ PhoneBook/Models/Person.cs
@@ -1,33 +1,41 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace PhoneBook.Models
 {
     public class Person
     {
+        [Key]
+        public int Id { get; set; }
         public string Name { get; set; }
         public string Family { get; set; }
+        public string City { get; set; }
+        public string SeniorExcutiveName { get; set; }
+        public string OfficeName { get; set; }
+        public string InternalTel { get; set; }
+        public bool IsActive { get; set; }
         public string PhoneNumber { get; set; }
         public string Address { get; set; }
 
 
 
 
-        public Person(string f, string l, string pn, string ad)
-        {
-            string name = f;
-            string family = l;
-            string phone = pn;
-            string address = ad;
-        }
+        //public Person(string f, string l, string pn, string ad)
+        //{
+        //    string name = f;
+        //    string family = l;
+        //    string phone = pn;
+        //    string address = ad;
+        //}
 
-        public Person()
-        {
-        }
+        //public Person()
+        //{
+        //}
 
-        internal static object Where(Func<object, bool> p)
-        {
-            throw new NotImplementedException();
-        }
+        //internal static object Where(Func<object, bool> p)
+        //{
+        //    throw new NotImplementedException();
+        //}
     }
 
 
