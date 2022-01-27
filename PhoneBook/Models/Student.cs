b79311f168683ccda567795f7de2d96d34c4c6c5
farshad <farshadnp6@gmail.com>
2022-01-27 04:57:26 +0330

    not important.

---
+++ PhoneBook/Models/Student.cs
@@ -0,0 +1,14 @@
+﻿namespace PhoneBook.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string InternalTel { get; set; }
+        public string Picture { get; set; }
+        public string Address { get; set; }
+    }
+}
